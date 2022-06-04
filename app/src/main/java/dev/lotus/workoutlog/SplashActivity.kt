e343b9a47f54cb03b0a05194c01947790a2df490
Mwihakirosemary <kakirosemary@gmal.com>
2022-06-04 08:22:41 +0300

    Splash Activity

--- app/src/main/java/dev/lotus/workoutlog/SplashActivity.kt
+++ app/src/main/java/dev/lotus/workoutlog/SplashActivity.kt
@@ -1,11 +1,15 @@
 package dev.lotus.workoutlog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val intent = Intent(this,LoginActivity::class.java)
+        startActivity(intent)
+        startActivity(intent)
+        finish()
     }
 }
