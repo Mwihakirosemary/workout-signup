2bb0268838fab3e769a2691f4937b1690e286e2e
Mwihakirosemary <kakirosemary@gmal.com>
2022-06-04 09:43:25 +0300

    Added login Activity

--- app/src/main/java/dev/lotus/workoutlog/LoginActivity.kt
+++ app/src/main/java/dev/lotus/workoutlog/LoginActivity.kt
@@ -2,10 +2,42 @@ package dev.lotus.workoutlog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var btnLogin: Button
+    lateinit var tilEmail: TextInputLayout
+    lateinit var tilPassword: TextInputLayout
+    lateinit var etEmail: TextInputEditText
+    lateinit var etPassword: TextInputEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnLogin = findViewById(R.id.btnLogin)
+        tilEmail = findViewById(R.id.tilEmail)
+        tilPassword = findViewById(R.id.tilPassword)
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+
+        btnLogin.setOnClickListener {
+            validateLogin()
+
+        }
+    }
+    fun validateLogin(){
+        var email = etEmail.text.toString()
+        var password = etPassword.text.toString()
+
+        if (email.isBlank()){
+            tilEmail.error = getString(R.string.email_required)
+        }
+
+        if (email.isBlank()){
+            tilPassword.error = getString(R.string.password_required)
+        }
+
+
     }
 }
